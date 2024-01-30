9e09709223ba7ba9f6a165205938d070f19f5670
kurodenjiro <kurodenjiro1@gmail.com>
2024-01-31 04:27:03 +0700

    update

--- app/reward/layout.tsx
+++ app/reward/layout.tsx
@@ -4,7 +4,7 @@ export default function RewardLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="h-full max-w-lg  mx-auto font-medium bg-slate-50 px-8">
+		<section className="h-full max-w-lg  mx-auto font-medium bg-slate-50 px-8 bg-no-repeat bg-cover" style={{backgroundImage: "url(/gotchi/Assets/Background.png)"}}>
 			{children}
 		</section>
 	);
